f3293c0c85985fb12b074ba169832b3ce84aa253
Rohit Bisht <74498290+R-ohit-B-isht@users.noreply.github.com>
2022-07-19 08:36:32 +0530

    Update mail.js

--- client/mail.js
+++ client/mail.js
@@ -6,7 +6,7 @@ async function mail(route) {
 
   var details=` Name : ${route.name} \n Phone Number : ${route.number} \n Email : ${route.email} \n Message : ${route.message} \n`;
   console.log(details)
-  await axios.post('http://localhost:4000/send_mail',{text:details})
+  await axios.post('https://reactnativenodemail.herokuapp.com/send_mail',{text:details})
 
 }
 
